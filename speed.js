62434f1a0ed9cdc71f34363b8dab3ab5fb8cb655
Joss2416 <36967170+Joss2416@users.noreply.github.com>
2018-11-08 00:35:35 +0700

    Add files via upload

---
+++ speed.js
@@ -0,0 +1,140 @@
+function I(id){return document.getElementById(id);}
+var meterBk="#E0E0E0";
+var dlColor="#6060AA",
+	ulColor="#309030",
+	pingColor="#AA6060",
+	jitColor="#AA6060";
+var progColor="#EEEEEE";
+
+//CODE FOR GAUGES
+function drawMeter(c,amount,bk,fg,progress,prog){
+	var ctx=c.getContext("2d");
+	var dp=window.devicePixelRatio||1;
+	var cw=c.clientWidth*dp, ch=c.clientHeight*dp;
+	var sizScale=ch*0.0055;
+	if(c.width==cw&&c.height==ch){
+		ctx.clearRect(0,0,cw,ch);
+	}else{
+		c.width=cw;
+		c.height=ch;
+	}
+	ctx.beginPath();
+	ctx.strokeStyle=bk;
+	ctx.lineWidth=16*sizScale;
+	ctx.arc(c.width/2,c.height-58*sizScale,c.height/1.8-ctx.lineWidth,-Math.PI*1.1,Math.PI*0.1);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.strokeStyle=fg;
+	ctx.lineWidth=16*sizScale;
+	ctx.arc(c.width/2,c.height-58*sizScale,c.height/1.8-ctx.lineWidth,-Math.PI*1.1,amount*Math.PI*1.2-Math.PI*1.1);
+	ctx.stroke();
+	if(typeof progress !== "undefined"){
+		ctx.fillStyle=prog;
+		ctx.fillRect(c.width*0.3,c.height-16*sizScale,c.width*0.4*progress,4*sizScale);
+	}
+}
+function mbpsToAmount(s){
+	return 1-(1/(Math.pow(1.3,Math.sqrt(s))));
+}
+function msToAmount(s){
+	return 1-(1/(Math.pow(1.08,Math.sqrt(s))));
+}
+
+//SPEEDTEST AND UI CODE
+var w=null; //speedtest worker
+var data=null; //data from worker
+var testParameters={
+	telemetry_level:"basic"
+	//Optional: add more test parameters here
+}
+function startStop(){
+	if(w!=null){
+		//speedtest is running, abort
+		w.postMessage('abort');
+		w=null;
+		data=null;
+		I("startStopBtn").className="";
+		initUI();
+	}else{
+		//test is not running, begin
+		w=new Worker('speedtest_worker.min.js');
+		w.postMessage('start {"telemetry_level":"basic"}');
+		I("startStopBtn").className="running";
+		I("shareArea").style.display="none";
+		w.onmessage=function(e){
+			data=JSON.parse(e.data);
+			var status=data.testState;
+			if(status>=4){
+				//test completed
+				I("startStopBtn").className="";
+				w=null;
+				updateUI(true);
+				if(status==4){
+					//if testId is present, show sharing panel, otherwise do nothing
+					try{
+						var testId=Number(data.testId);
+						if(!isNaN(testId)){
+							var shareURL=window.location.href.substring(0,window.location.href.lastIndexOf("/"))+"/results/?id="+testId;
+							I("resultsImg").src=shareURL;
+							I("resultsURL").value=shareURL;
+							I("testId").innerHTML=testId;
+							I("shareArea").style.display="block";
+						}
+					}catch(e){}
+				}
+			}
+		};
+	}
+}
+//this function reads the data sent back by the worker and updates the UI
+function updateUI(forced){
+	if(!forced&&(!data||!w)) return;
+	var status=data.testState;
+	I("ip").textContent=data.clientIp;
+	I("dlText").textContent=(status==1&&data.dlStatus==0)?"...":data.dlStatus;
+	drawMeter(I("dlMeter"),mbpsToAmount(Number(data.dlStatus*(status==1?oscillate():1))),meterBk,dlColor,Number(data.dlProgress),progColor);
+	I("ulText").textContent=(status==3&&data.ulStatus==0)?"...":data.ulStatus;
+	drawMeter(I("ulMeter"),mbpsToAmount(Number(data.ulStatus*(status==3?oscillate():1))),meterBk,ulColor,Number(data.ulProgress),progColor);
+	I("pingText").textContent=data.pingStatus;
+	drawMeter(I("pingMeter"),msToAmount(Number(data.pingStatus*(status==2?oscillate():1))),meterBk,pingColor,Number(data.pingProgress),progColor);
+	I("jitText").textContent=data.jitterStatus;
+	drawMeter(I("jitMeter"),msToAmount(Number(data.jitterStatus*(status==2?oscillate():1))),meterBk,jitColor,Number(data.pingProgress),progColor);
+}
+function oscillate(){
+	return 1+0.02*Math.sin(Date.now()/100);
+}
+//poll the status from the worker (this will call updateUI)
+setInterval(function(){
+	if(w) w.postMessage('status');
+},200);
+//update the UI every frame
+window.requestAnimationFrame=window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.msRequestAnimationFrame||(function(callback,element){setTimeout(callback,1000/60);});
+function frame(){
+	requestAnimationFrame(frame);
+	updateUI();
+}
+frame(); //start frame loop
+//function to (re)initialize UI
+function initUI(){
+	/*if(!(window.parent===window)){
+		document.body.innerHTML="Please do not embed this demo page. Install HTML5 Speedtest on your server instead.";
+		return;
+	}*/
+	drawMeter(I("dlMeter"),0,meterBk,dlColor,0);
+	drawMeter(I("ulMeter"),0,meterBk,ulColor,0);
+	drawMeter(I("pingMeter"),0,meterBk,pingColor,0);
+	drawMeter(I("jitMeter"),0,meterBk,jitColor,0);
+	I("dlText").textContent="";
+	I("ulText").textContent="";
+	I("pingText").textContent="";
+	I("jitText").textContent="";
+	I("ip").textContent="";
+}
+
+{
+	var x=new XMLHttpRequest();
+	x.open("GET","counter.php?r="+Math.random(),true);
+	x.send();
+}
+
+</script>
